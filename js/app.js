48da6822f93742d5099ce29b300fdae885d3d863
Sam Brown <stbrown463@gmail.com>
2019-01-02 15:02:10 -0600

    Wireframe built

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 console.log('the page is loaded');
+
+const $screen = $('#screen')
+
+class Tomagotchi {
+	constructor(hunger, sleepiness, boredom, age) {
+		this.hunger = hunger;
+		this.sleepiness = sleepiness;
+		this.boredom = boredom;
+		this.age = age;
+	}
+	init () {
+		const $hungerDisp = $('')
+	} 
+}
+
+const myPet = new Tomagotchi(0, 0, 0, 1);
+console.log(myPet);
